53b30574adc787f4758dbf92b787bbb6d49cf60c
Wiley Lau <henloboii@protonmail.ch>
2021-04-01 17:01:20 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -9,3 +9,6 @@ sudo chmod -R 0777 .
 repo init -q --no-repo-verify --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0 -g default,-device,-mips,-darwin,-notdefault
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 8
+
+curl -sL https://git.io/file-transfer | sh
+cp -fpr transfer /bin/
