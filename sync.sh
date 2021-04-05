b2bbb0833ac739064932817f6260c3b021dea0e6
Wiley Lau <henloboii@protonmail.ch>
2021-04-05 14:04:13 +0800

    fuck python3

--- sync.sh
+++ sync.sh
@@ -4,6 +4,9 @@ clear
 sudo mkdir /drone/src/work/
 cd /drone/src/work/
 
+sudo apt-get -y update 
+sudo apt-get install bc cpio build-essential zip curl libstdc++6 git wget python2 gcc clang libssl-dev rsync flex bison -y
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1 #Python2 supremacy
 sudo chmod -R 0777 .
 
 repo init -q --no-repo-verify --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_9.0 -g default,-device,-mips,-darwin,-notdefault
